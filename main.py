5c04c16fa220ccfbad2ad7395b5e6fdcfca63e12
Mordor828 <tranm98@gmail.com>
2025-05-12 13:22:24 +0200

    new files

---
+++ main.py
@@ -0,0 +1,45 @@
+# Python Baking App
+
+def show_balance():
+    print(f"Your balance is ${balance:.2f}")
+
+def deposit():
+    amount = float(input("Enter a amount to be deposited: "))
+
+    if amount < 0:
+        print("That's not a valid amount")
+        return 0
+    else:
+        return amount
+
+def withdraw():
+    pass
+
+balance = 0
+is_running = True
+
+
+
+while is_running:
+    print("Banking Program")
+    print("1.Show Balance")
+    print("2.Deposit")
+    print("3.Withdraw")
+    print("4.Exit")
+
+    choice = input("Enter your choice (1-4):")
+
+    if choice == '1':
+        show_balance()
+    elif choice == '2':
+        balance += deposit()
+    elif choice == '3':
+        withdraw()
+    elif choice == '4':
+        is_running = False
+    else:
+        print("That is not a valid choice")
+
+    
+print("Thank you! Have a nice day!")
+
